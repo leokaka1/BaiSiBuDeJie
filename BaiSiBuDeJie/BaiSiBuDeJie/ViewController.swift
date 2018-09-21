20ede51a58686687bd603a85e8e2be81fcea183a
leokaka1 <leocaoxiaozhu@163.com>
2018-09-21 09:00:22 +0800

    important
    Basic fundation

---
+++ BaiSiBuDeJie/BaiSiBuDeJie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiSiBuDeJie
+//
+//  Created by 曹笑竹 on 2018/9/21.
+//  Copyright © 2018年 曹笑竹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
